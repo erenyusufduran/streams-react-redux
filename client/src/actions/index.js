bea62e180e8be1f6b89831f770da9d072d017145
Eren Yusuf Duran <erenydurann@gmail.com>
2022-03-03 00:45:46 +0300

    editStream&refactorsAboutStreamPages

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -28,7 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: CREATE_STREAM, payload: response.data });
   // Do some programmatic navigation to get the user back to the root route
-  history.push('/');
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -42,8 +42,9 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = (id) => async (dispatch) => {
